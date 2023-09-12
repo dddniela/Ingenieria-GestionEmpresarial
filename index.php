597b2e1eef1babec1ef44a9d119a8819ef37fe39
DanielaMendezAr <danielamendez0809@gmail.com>
2023-09-11 22:01:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $comunidad->setConnection($conn->getDB());
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Ingeniería, Gestión, Empresarial, Ingeniería en Gestión Empresarial">
-    <meta name="author" content="Instituto Tecnológico de Veracruz">
+    <meta name="author" content="Tecnológico Nacional de México Veracruz">
     <link rel="icon" href="img/itver-logo.PNG" />
 
     <link rel="stylesheet" href="assets/css/bootstrap.css">
